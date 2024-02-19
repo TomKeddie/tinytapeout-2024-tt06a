14da8195d11c90444c3f68b2cdaf4030cf7bc1b0
Tom Keddie <github@bronwenandtom.com>
2024-02-19 09:56:15 -0800

    Move sim to test directory

--- test/tb.v
+++ test/tb.v
@@ -1,44 +1,52 @@
-`default_nettype none `timescale 1ns / 1ps
+`default_nettype none
+`timescale 1ns/1ps
 
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
-*/
-module tb ();
+module tb (
+           // testbench is controlled by test.py
+	       input  clk,
+	       input  rst,
+	       output r0,
+	       output r1,
+	       output r2,
+	       output r3,
+	       output g0,
+	       output g1,
+	       output g2,
+	       output g3,
+	       output b0,
+	       output b1,
+	       output b2,
+	       output b3,
+	       output hs,
+	       output vs
+           );
 
-  // Dump the signals to a VCD file. You can view it with gtkwave.
+  // this part dumps the trace to a vcd file that can be viewed with GTKWave
   initial begin
-    $dumpfile("tb.vcd");
-    $dumpvars(0, tb);
+    $dumpfile ("tb.vcd");
+    $dumpvars (0, tb.clk);
+    $dumpvars (0, tb.rst);
+    $dumpvars (0, tb.vga.count_h);
+    $dumpvars (0, tb.vga.count_v);
+    
     #1;
   end
 
-  // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
-
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
-
-      // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(1'b1),
-      .VGND(1'b0),
-`endif
-
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
-  );
-
+  // instantiate the DUT
+  vga vga(.clk(clk), 
+	      .rst(rst),
+	      .r0(r0),
+	      .r1(r1),
+	      .r2(r2),
+	      .r3(r3),
+	      .b0(b0),
+	      .b1(b1),
+	      .b2(b2),
+	      .b3(b3),
+	      .g0(g0),
+	      .g1(g1),
+	      .g2(g2),
+	      .g3(g3),
+	      .hs(hs),
+	      .vs(vs));
 endmodule
--- test/test.py
+++ test/test.py
@@ -1,32 +1,19 @@
-# SPDX-FileCopyrightText: © 2023 Uri Shaked <uri@tinytapeout.com>
-# SPDX-License-Identifier: MIT
-
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
 @cocotb.test()
-async def test_adder(dut):
-  dut._log.info("Start")
-  
-  # Our example module doesn't use clock and reset, but we show how to use them here anyway.
-  clock = Clock(dut.clk, 10, units="us")
-  cocotb.start_soon(clock.start())
+async def top(dut):
+    dut._log.info("start")
+    # 25.175MHz => 0.000_000_039_722s
+    clock = Clock(dut.clk, 39722, units="ps")
+    cocotb.start_soon(clock.start())
+    
+    dut._log.info("reset")
+    dut.rst.value = 1
+    await ClockCycles(dut.clk, 10)
+    dut.rst.value = 0
 
-  # Reset
-  dut._log.info("Reset")
-  dut.ena.value = 1
-  dut.ui_in.value = 0
-  dut.uio_in.value = 0
-  dut.rst_n.value = 0
-  await ClockCycles(dut.clk, 10)
-  dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 500)
 
-  # Set the input values, wait one clock cycle, and check the output
-  dut._log.info("Test")
-  dut.ui_in.value = 20
-  dut.uio_in.value = 30
-
-  await ClockCycles(dut.clk, 1)
-
-  assert dut.uo_out.value == 50
+    await ClockCycles(dut.clk, 450000)
